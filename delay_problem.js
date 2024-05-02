b18a2f020b687756774af05218a9bbacdbd2233c
Basiq-Naeem-Ocloud <165660236+Basiq-Naeem-Ocloud@users.noreply.github.com>
2024-05-02 17:57:43 +0500

    "delay problem solution"

---
+++ delay_problem.js
@@ -0,0 +1,24 @@
+
+function delay(num)
+{
+    return new Promise(function (resolve,reject)
+    {
+        setTimeout(() => {
+            resolve('added delay');
+        }, num);
+    });
+}
+
+async function delayprob()
+{
+    console.log("action1")
+    await delay(2000)
+    console.log("action2")
+
+    await delay(2000)
+    console.log("action3")
+}
+
+
+
+delayprob();
